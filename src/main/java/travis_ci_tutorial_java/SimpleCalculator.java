ef35cb1d7e54dc0dd9e2ccaa83734f66463ff253
benya <benya@DESKTOP-SCC7UV6.myfiosgateway.com>
2021-12-13 03:39:41 -0500

    update

--- src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
+++ src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
@@ -1,7 +1,22 @@
 package travis_ci_tutorial_java;
 
 public class SimpleCalculator {
-	public int add(int a, int b) {
+	/*public int add(int a, int b) {
 		return a + b;
+	}*/
+	
+	public int minus(int a, int b) {
+		return a - b;
 	}
+/*
+	public int multiply(int a, int b) {
+		return a * b;
+	}
+	public double divide(double a, double b) {
+		if(b==0) {
+			throw new IllegalArgumentException("Number can not be divide by 0!");
+			
+		}
+		return a / b;
+	}*/
 }
